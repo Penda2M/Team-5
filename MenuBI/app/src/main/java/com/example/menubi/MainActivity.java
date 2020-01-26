9c9bb296df2f073aae299ab123fe9262b3dee44a
Penda <ndao31114@gmail.com>
2020-01-26 22:58:26 +0000

    ajout modifications

--- MenuBI/app/src/main/java/com/example/menubi/MainActivity.java
+++ MenuBI/app/src/main/java/com/example/menubi/MainActivity.java
@@ -12,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     Button btnSignup, btnSignin;
     TextView txtSlogan;
 
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
